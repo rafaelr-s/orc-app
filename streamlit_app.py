b8aedae967b645621ae4cfef0930f43609e203ea
rafaelr-s <rafaelrnlog@gmail.com>
2025-10-01 15:04:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1004,3 +1004,14 @@ if menu == "Histórico de Orçamentos":
                             mime="application/pdf",
                             key=f"download_historico_{orc_id}"
                         )
+                        with col3:
+                        if st.button("❌ Excluir", key=f"excluir_{orc_id}"):
+                            conn = sqlite3.connect(DB_NAME) 
+                            cur = conn.cursor()
+                            cur.execute("DELETE FROM orcamentos WHERE id=?", (orc_id,))
+                            cur.execute("DELETE FROM itens_confeccionados WHERE orcamento_id=?", (orc_id,))
+                            cur.execute("DELETE FROM itens_bobinas WHERE orcamento_id=?", (orc_id,))
+                            conn.commit()
+                            conn.close()
+                            st.success(f"Orçamento ID {orc_id} excluído!")
+                            st.rerun()
